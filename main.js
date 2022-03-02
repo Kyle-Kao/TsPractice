2faf8197dfbdd91942aedf312966b18d0de71c5e
kylekao <kyle.kao@itnsl.net>
2022-03-02 10:45:04 +0800

    add number

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 "use strict";
-let input1 = document.querySelector('#num1');
-let input2 = document.querySelector('#num2');
-let sum = document.querySelector('#sum');
-let add = (num1, num2) => {
-    return num1 + num2;
-};
-sum.addEventListener('click', () => {
-    console.log(add(+input1.value, +input2.value));
-});
+function add(n1, n2, showResult, phrase) {
+    if (showResult) {
+        let res = (n1 + n2).toFixed(2);
+        console.log(phrase + res);
+    }
+    return n1 + n2;
+}
+const number1 = 5;
+const number2 = 2.8845682;
+const printResult = true;
+const resultPhrase = 'Result is: ';
+add(number1, number2, printResult, resultPhrase);
--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
-let input1 = document.querySelector('#num1')! as HTMLInputElement
-let input2 = document.querySelector('#num2')! as HTMLInputElement
-let sum = document.querySelector('#sum')!as HTMLInputElement
-
-let add = (num1: number, num2: number)=>{
-  return num1 + num2 
+function add(n1:number,n2:number, showResult: boolean, phrase: string){
+  if(showResult){
+    let res = (n1 +n2).toFixed(2)
+    console.log(phrase + res)
+  }
+  return n1 + n2
 }
-
-sum.addEventListener('click',()=>{
-  console.log(add(+input1.value, +input2.value))
-})
+const number1 = 5;
+const number2 = 2.8845682
+const printResult = true
+const resultPhrase = 'Result is: '
+add(number1,number2,printResult,resultPhrase)
